65b2ca65818262e2b020fd51930342d263e8327c
EzekielRice <98047110+ricee07@users.noreply.github.com>
2022-01-25 17:38:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+food = ["Burger", "Shake", "Fries"]
+print(food[0], "is located at index 0.")
+print(food[1], "is located at index 1.")
+print(food[2], "is located at index 2.")
+if ("Burger" in food):
+  print("One Burger Coming Up")
+else:
+  print("Sorry Burgers are not availible") 
+if ("Shake" in food):
+  print("One Shake Coming Up")
+else:
+  print("Sorry Fries are not availible") 
+if ("Burger" in food):
+  print("One Fry Coming Up")
+else:
+  print("Sorry Fries are not availible") 
+if ("Pizza" in food):
+  print("One Slice Coming Up")
+else:
+  print("Sorry Pizza is not availible") 
